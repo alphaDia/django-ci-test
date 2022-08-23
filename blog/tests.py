77f6915fc40b681e2f23bf98c176dde413082233
Mamadou alpha diallo <alphadiallo2235@gmail.com>
2022-08-22 17:46:11 -0700

    ci settled

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Blog
 # Create your tests here.
+
+
+class POSTTestCase(TestCase):
+
+    def setUp(self):
+        self.blog = Blog.objects.create(title='django', author='harouna', slug='django')
+
+    def test_post_blog(self):
+        d = self.blog
+        self.assertTrue(isinstance(self.blog, Blog))
+        self.assertEqual(str(d), 'django')
